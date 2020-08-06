a3ee6eb5f598bd5418a9ef430b75c3fe2338f095
Quang Huu Nguyen <50345722+totenk0pf@users.noreply.github.com>
2020-08-07 01:13:45 +1200

    Bug fixes

--- nhen.py
+++ nhen.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 
 client = commands.Bot(command_prefix='.')
 LINK = "https://nhentai.net/"
-TOKEN = ""
+TOKEN = "NzQwODA2MTc5MjE4OTE1NDA5.XyuXhA.P8UNdagobZirSLjgpbbCGvrdNMc"
 
 class Henpy():
     """Main class for Henpy requests and data handling.
@@ -56,8 +56,14 @@ class Henpy():
         for i in tags:
             self.search_link += i + "+"
         self.get_request(self.search_link)
-        self.last = int(self.data.find("a", {"class":"last"})["href"].split("=")[-1])
-        self.get_request(self.search_link + str(random.randrange(1, self.last)))
+        try:
+            self.last = int(self.data.find("a", {"class":"last"})["href"].split("=")[-1])
+        except:
+            self.last = 1
+        if self.last != 1:
+            self.get_request(self.search_link + "&page=" + str(random.randrange(1, self.last)))
+        else:
+            self.get_request(self.search_link)
         self.found_codes = self.data.find_all("a", {"class":"cover"})
         self.esc_list = []
         for i in self.found_codes:
@@ -80,21 +86,24 @@ async def sex(ctx, code):
     Args:
         code ([int, "random"]): 6-digit code for whatever you desire to beat your meat to, or put "random" in to test your luck.
     """
-    if ctx.author.id not in henpy.id_ban:
-        f_data = henpy.get_page(code)
-        if f_data == False:
-            await ctx.send("Illegal content detected, retrying...")
-            f_data = henpy.get_page("random")
-        desc = "Tags: "
-        for i in f_data[3]:
-            desc += "[%s](%s), " % (i, ("https://nhentai.net/tag/" + i.replace(" ", "-")))
-        embed = discord.Embed(title=f_data[0], url=f_data[1], description=desc, color=0xf50000)
-        embed.set_author(name="nHenpy", icon_url="https://cdn.discordapp.com/avatars/740806179218915409/6b4da565bce7050f089e5d5ae6ab69f8.png?size=128")
-        embed.set_footer(text="powered by sex.")
-        embed.set_image(url=f_data[2])
-        await ctx.send(embed=embed)
-    else:
-        await ctx.send("CON CHÓ DƯƠNG NHẬT DUY CƯỠNG DÂM TRẺ EM, BẮT CÓC HỌC SINH TIỂU HỌC!")
+    try:
+        if ctx.author.id not in henpy.id_ban:
+            f_data = henpy.get_page(code)
+            if f_data == False:
+                await ctx.send("Illegal content detected, retrying...")
+                f_data = henpy.get_page("random")
+            desc = "Tags: "
+            for i in f_data[3]:
+                desc += "[%s](%s), " % (i, ("https://nhentai.net/tag/" + i.replace(" ", "-")))
+            embed = discord.Embed(title=f_data[0], url=f_data[1], description=desc, color=0xf50000)
+            embed.set_author(name="nHenpy", icon_url="https://cdn.discordapp.com/avatars/740806179218915409/6b4da565bce7050f089e5d5ae6ab69f8.png?size=128")
+            embed.set_footer(text="powered by sex.")
+            embed.set_image(url=f_data[2])
+            await ctx.send(embed=embed)
+        else:
+            await ctx.send("CON CHÓ DƯƠNG NHẬT DUY CƯỠNG DÂM TRẺ EM, BẮT CÓC HỌC SINH TIỂU HỌC!")
+    except Exception as e:
+        print(e)
 
 @client.command()
 async def tags(ctx, *args:str):
@@ -105,27 +114,33 @@ async def tags(ctx, *args:str):
     Args:
         *args (str): Tags to look for.
     """
-    if ctx.author.id not in henpy.id_ban:
-        code_list = []
-        for i in args:
-            code_list.append(i)
-        f_data = henpy.get_page_by_tags(code_list)
-        desc = "Tags: "
-        for i in f_data[3]:
-            desc += "[%s](%s), " % (i, ("https://nhentai.net/tag/" + i.replace(" ", "-")))
-        embed = discord.Embed(title=f_data[0], url=f_data[1], description=desc, color=0xf50000)
-        embed.set_author(name="nHenpy", icon_url="https://cdn.discordapp.com/avatars/740806179218915409/6b4da565bce7050f089e5d5ae6ab69f8.png?size=128")
-        embed.set_footer(text="powered by sex.")
-        embed.set_image(url=f_data[2])
-        await ctx.send(embed=embed)
-    else:
-        await ctx.send("CON CHÓ DƯƠNG NHẬT DUY CƯỠNG DÂM TRẺ EM, BẮT CÓC HỌC SINH TIỂU HỌC!")
+    try:
+        if ctx.author.id not in henpy.id_ban:
+            code_list = []
+            for i in args:
+                code_list.append(i)
+            f_data = henpy.get_page_by_tags(code_list)
+            if f_data == False:
+                await ctx.send("Illegal content detected, retrying...")
+                f_data = henpy.get_page_by_tags(code_list)
+            desc = "Tags: "
+            for i in f_data[3]:
+                desc += "[%s](%s), " % (i, ("https://nhentai.net/tag/" + i.replace(" ", "-")))
+            embed = discord.Embed(title=f_data[0], url=f_data[1], description=desc, color=0xf50000)
+            embed.set_author(name="nHenpy", icon_url="https://cdn.discordapp.com/avatars/740806179218915409/6b4da565bce7050f089e5d5ae6ab69f8.png?size=128")
+            embed.set_footer(text="powered by sex.")
+            embed.set_image(url=f_data[2])
+            await ctx.send(embed=embed)
+        else:
+            await ctx.send("CON CHÓ DƯƠNG NHẬT DUY CƯỠNG DÂM TRẺ EM, BẮT CÓC HỌC SINH TIỂU HỌC!")
+    except Exception as e:
+        print(e)
 
 @client.event
 async def on_command_error(ctx, error):
     if isinstance(error, commands.MissingRequiredArgument):
-        if error.param.name == "cap":
-            pass
+        if error.param.name == "code":
+            await ctx.send("What the fuck are you trying to do?")
     if isinstance(error, commands.BadArgument):
         await ctx.send("Wrong argument!")
 
